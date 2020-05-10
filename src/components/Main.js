6ef0eb1da517d765722588eb3ac5fafdecbb84ea
Brian Johnston <brian.johnston96@gmail.com>
2020-05-10 22:17:51 +0100

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+function Main() {
+  return (
+    <div>
+      <TodoItem text="fuckyou" />
+      <TodoItem text="bollocks" />
+      <TodoItem text="test" />
+    </div>
+  );
+}
+
+export default Main;
